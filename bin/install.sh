babf9e9b437c74797a7501d08af5d2135de2e98f
chibashr <mrdumez@gmail.com>
2026-02-03 16:16:31 -0600

    Enhance WebSocket configuration in scripts and update CSS for live update log
    This update adds additional proxy headers and timeout settings for WebSocket connections in both the apply-web-permissions and install scripts, improving connection handling. Additionally, the CSS for the live update log has been updated to utilize system theme colors, enhancing visual consistency across themes.

--- bin/install.sh
+++ bin/install.sh
@@ -1221,9 +1221,13 @@ server {
     location /ws/ {
         proxy_pass http://127.0.0.1:5000;
         proxy_http_version 1.1;
+        proxy_set_header Host $host;
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection "upgrade";
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_cache_bypass $http_upgrade;
         proxy_read_timeout 86400;
+        proxy_send_timeout 86400;
     }
 }
 EOF
